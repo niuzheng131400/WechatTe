6abc592044a8a0cb5e7a180e297ffdbe015184c9
niuzheng131400 <771036148@qq.com>
2022-10-14 16:38:21 +0800

    提交

--- WechatTe/Plugin.php
+++ WechatTe/Plugin.php
@@ -31,7 +31,7 @@ class WechatTe_Plugin implements Typecho_Plugin_Interface
 
     private static function init()
     {
-        $db     = Typecho_Db::get();
+        $db = Typecho_Db::get();
         $prefix = $db->getPrefix();
         if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
             $db->query('ALTER TABLE `' . $prefix . 'contents` ADD `views` INT(10) DEFAULT 0;');
@@ -52,8 +52,8 @@ class WechatTe_Plugin implements Typecho_Plugin_Interface
         $default_swiper = '[{"link":"/pages/cate/cate?cate=361","cover":"https://img.xxxxx.net/usr/uploads/2020/09/2185816269.png"},{"link":"/pages/cate/cate?cate=363","cover":"https://img.xxxxx.net/usr/uploads/2020/09/573794959.jpg"},{"link":"/pages/cate/cate?cate=2","cover":"https://img.xxxxx.net/usr/uploads/images/php-default.jpg"},{"link":"/pages/cate/cate?cate=49","cover":"https:/xxxx.net/usr/uploads/images/xiaojiqiao.jpeg"}]';
         $swiper = new Typecho_Widget_Helper_Form_Element_Textarea('swiper', NULL, $default_swiper, _t('首页轮播图'), _t('url里面的cate为分类的mid'));
         $form->addInput($swiper);
-		
-		$hide_category = new Typecho_Widget_Helper_Form_Element_Text('hide_category', NULL, '', _t('隐藏分类ID'), _t('多个分类ID以英文逗号分隔,用于小程序分类列表'));
+
+        $hide_category = new Typecho_Widget_Helper_Form_Element_Text('hide_category', NULL, '', _t('隐藏分类ID'), _t('多个分类ID以英文逗号分隔,用于小程序分类列表'));
         $form->addInput($hide_category);
 
         $app_secret = new Typecho_Widget_Helper_Form_Element_Text('app_secret', NULL, 'xxx', _t('api密钥'), _t('要与小程序端/helpers/md5.js中app_secret字段保持一致，否则无法从服务器读取数据'));
@@ -134,7 +134,11 @@ class WechatTe_Plugin implements Typecho_Plugin_Interface
         Helper::configPlugin('WechatTe', $config);
     }
 
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
 
-    public static function render(){}
+    public static function render()
+    {
+    }
 }
